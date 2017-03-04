b91bb13ffecab36fd5ccbf354df47403ea8a7f03
syamnich <sergey.yamnich@gmail.com>
2017-03-04 16:14:39 +0200

    Add generators for haml and rspec

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module TestApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec
+    end
   end
 end
